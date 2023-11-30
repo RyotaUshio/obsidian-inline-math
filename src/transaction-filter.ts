5ec1eb0e86cf924fe1b8cee3571867b3d7b8a2c3
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-12-01 01:45:23 +0900

    no longer treat the whole document string for insertion & selection

--- src/transaction-filter.ts
+++ src/transaction-filter.ts
@@ -33,7 +33,6 @@ export const makeTransactionFilter = (plugin: NoMoreFlicker): Extension => {
 
 
 export function getChangesForDeletion(state: EditorState): ChangeSpec {
-
     const tree = syntaxTree(state);
     const doc = state.doc.toString();
     const changes: ChangeSpec[] = [];
@@ -54,12 +53,9 @@ export function getChangesForDeletion(state: EditorState): ChangeSpec {
             from: indexPrevDollar,
             to: indexNextDollar >= 0 ? indexNextDollar : to,
             enter(node) {
-                // printNode(node, state);
-                if (isInlineMathBegin(node, state)
-                    && state.sliceDoc(node.to, node.to + 3) == "{} ") {
+                if (isInlineMathBegin(node, state) && state.sliceDoc(node.to, node.to + 3) == "{} ") {
                     changes.push({ from: node.to, to: node.to + 3 });
-                } else if (isInlineMathEnd(node, state)
-                    && state.sliceDoc(node.from - 3, node.from) == " {}") {
+                } else if (isInlineMathEnd(node, state) && state.sliceDoc(node.from - 3, node.from) == " {}") {
                     changes.push({ from: node.from - 3, to: node.from });
                 }
             }
@@ -72,7 +68,6 @@ export function getChangesForDeletion(state: EditorState): ChangeSpec {
 
 export function getChangesForInsertion(state: EditorState, changes: ChangeSet): ChangeSpec {
     const tree = syntaxTree(state);
-    const doc = state.doc.toString();
     const changesToAdd: ChangeSpec[] = [];
 
     const beginningOfChanges: Map<number, boolean> = new Map();
@@ -82,9 +77,11 @@ export function getChangesForInsertion(state: EditorState, changes: ChangeSet): 
 
     for (const range of state.selection.ranges) {
         if (range.from >= 1) {
+            // "range.from >= 1" is necessary because
             // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf#parameters
             // "If position is less than 0, the behavior is the same as for 0"
-            const indexPrevDollar = doc.lastIndexOf("$", range.from - 1);
+            const line = state.doc.lineAt(range.from);
+            const indexPrevDollar = line.from + line.text.lastIndexOf("$", range.from - 1 - line.from);
 
             if (indexPrevDollar >= 0) {
                 const node = tree.cursorAt(indexPrevDollar, 1).node;
@@ -93,21 +90,19 @@ export function getChangesForInsertion(state: EditorState, changes: ChangeSet): 
                         // without this, 
                         // inserting "a" between the leading "$" and "x" of "$x$" results in "$a{} x {}$"
                         changesToAdd.push({ from: indexPrevDollar, to: range.from, insert: "${} " });
-                        continue;
-                    }
-
-                    if (state.sliceDoc(node.to, node.to + 3) !== "{} ") {
+                    } else if (state.sliceDoc(node.to, node.to + 3) !== "{} ") {
                         changesToAdd.push({ from: node.to, insert: "{} " });
                     }
                 }
                 else if (isInlineMathEnd(node, state) && state.sliceDoc(node.from - 3, node.from) === " {}") {
-                    const openIndex = doc.lastIndexOf("${} ", node.from - 3);
-                    changesToAdd.push({ from: openIndex + 1, to: node.from, insert: doc.slice(openIndex + 4, node.from - 3).trim() });
+                    const openIndex = line.from + line.text.lastIndexOf("${} ", node.from - 3 - line.from);
+                    changesToAdd.push({ from: openIndex + 1, to: node.from, insert: state.sliceDoc(openIndex + 4, node.from - 3).trim() });
                 }
             }
         }
 
-        const indexNextDollar = doc.indexOf("$", range.to);
+        const line = state.doc.lineAt(range.to);
+        const indexNextDollar = line.from + line.text.indexOf("$", range.to - line.from);
         if (indexNextDollar >= 0) {
             const node = tree.cursorAt(indexNextDollar, 1).node;
             if (isInlineMathEnd(node, state)) {
@@ -116,9 +111,9 @@ export function getChangesForInsertion(state: EditorState, changes: ChangeSet): 
                 }
             }
             else if (isInlineMathBegin(node, state) && state.sliceDoc(node.to, node.to + 3) === "{} ") {
-                const closeIndex = doc.indexOf(" {}$", node.to + 3);
+                const closeIndex = line.from + line.text.indexOf(" {}$", node.to + 3 - line.from,);
                 if (closeIndex >= 0) {
-                    changesToAdd.push({ from: node.to, to: closeIndex + 3, insert: doc.slice(node.to + 3, closeIndex).trim() });
+                    changesToAdd.push({ from: node.to, to: closeIndex + 3, insert: state.sliceDoc(node.to + 3, closeIndex).trim() });
                 }
             }
         }
@@ -130,28 +125,31 @@ export function getChangesForInsertion(state: EditorState, changes: ChangeSet): 
 
 export function getChangesForSelection(state: EditorState, newSelection: EditorSelection): ChangeSpec {
     const tree = syntaxTree(state);
-    const doc = state.doc.toString();
     const changes: ChangeSpec[] = [];
 
     for (let i = 0; i < newSelection.ranges.length; i++) {
         const range = newSelection.ranges[i];
-        const indexNextDollar = doc.indexOf("$", range.to);
-        const indexPrevDollar = doc.lastIndexOf("$", range.from - 1);
+
+        const lineFrom = state.doc.lineAt(range.from);
+        const lineTo = state.doc.lineAt(range.to);
+
+        const indexNextDollar = lineTo.from + lineTo.text.indexOf("$", range.to - lineTo.from);
+        const indexPrevDollar = lineFrom.from + lineFrom.text.lastIndexOf("$", range.from - 1 - lineFrom.from);
 
         if (indexPrevDollar >= 0) {
             const node = tree.cursorAt(indexPrevDollar, 1).node;
             if (isInlineMathEnd(node, state) && state.sliceDoc(node.from - 3, node.from) === " {}") {
-                const openIndex = doc.lastIndexOf("${} ", node.from - 3);
-                changes.push({ from: openIndex + 1, to: node.from, insert: doc.slice(openIndex + 4, node.from - 3).trim() });
+                const openIndex = lineFrom.from + lineFrom.text.lastIndexOf("${} ", node.from - 3 - lineFrom.from);
+                changes.push({ from: openIndex + 1, to: node.from, insert: state.sliceDoc(openIndex + 4, node.from - 3).trim() });
             }
         }
 
         if (indexNextDollar >= 0) {
             const node = tree.cursorAt(indexNextDollar, 1).node;
             if (isInlineMathBegin(node, state) && state.sliceDoc(node.to, node.to + 3) === "{} ") {
-                const closeIndex = doc.indexOf(" {}$", node.to + 3);
+                const closeIndex = lineTo.from + lineTo.text.indexOf(" {}$", node.to + 3 - lineTo.from);
                 if (closeIndex >= 0) {
-                    changes.push({ from: node.to, to: closeIndex + 3, insert: doc.slice(node.to + 3, closeIndex).trim() });
+                    changes.push({ from: node.to, to: closeIndex + 3, insert: state.sliceDoc(node.to + 3, closeIndex).trim() });
                 }
             }
         }
